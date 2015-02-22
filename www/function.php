9b348092d6130c6d686fde2543f5edd83c22e1f8
Серый <jorch1994@mail.ru>
2015-02-22 17:07:02 +1000

    Домашняя работа, работа с ООП

--- www/function.php
+++ www/function.php
@@ -1,9 +1,23 @@
 <?php
+class NewsPaper
+{
+    public $title;
+    public $text;
+    public $author;
+    public $data;
 
-function get_news(){
-    $fp = fopen('comments.txt','a');
-    fwrite($fp, $_POST['content']. "<br/>");
-    fclose($fp);
+    function DbConnect()
+    {
+        $db = mysql_connect("localhost","Admins","12345");
+        mysql_select_db("Newsis",$db);
+
+        $result = mysql_query("SELECT * FROM  news",$db);
+        $myrows = mysql_fetch_array($result);
+
+
+        $this->text = $myrows['text'];
+        $this->title = $myrows['title'];
+        $this->author = $myrows['author'];
+    }
 }
-
 ?>
