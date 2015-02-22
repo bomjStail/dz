301eb8c94ae4c85109a531f8ab6ad0e9dd314be4
Серый <jorch1994@mail.ru>
2015-02-22 17:07:19 +1000

    Домашняя работа, работа с ООП

--- www/index.php
+++ www/index.php
@@ -1,12 +1,5 @@
 <?php
-require('function.php');
-if($_POST['content']!=null){
-    get_news();
-}
-?>
-<h1>НОВОСНАЯ ЛЕНТА</h1>
-<?php
-echo nl2br(file_get_contents('comments.txt'));
+require(__DIR__.'function.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,13 +8,6 @@ echo nl2br(file_get_contents('comments.txt'));
     <title>news</title>
 </head>
 <body>
-
-<h3>Введит новость</h3>
-<form action="index.php" method="post">
-    <textarea name="content" rows="3" cols="100"></textarea>
-    <br/>
-    <input type="submit" value="post" />
-</form>
-
+$NewsPaper = new NewsPaper(text,title,author);
 </body>
 </html>
